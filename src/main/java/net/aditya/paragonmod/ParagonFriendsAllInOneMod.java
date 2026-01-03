f0468991d803a7b22fbe2ab4c411fb47abbc81fa
Aditya <adityakxd@gmail.com>
2026-01-03 00:48:48 -0500

    first item initialized!!

--- src/main/java/net/aditya/paragonmod/ParagonFriendsAllInOneMod.java
+++ src/main/java/net/aditya/paragonmod/ParagonFriendsAllInOneMod.java
@@ -1,5 +1,6 @@
 package net.aditya.paragonmod;
 
+import net.aditya.paragonmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ public class ParagonFriendsAllInOneMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/aditya/paragonmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.aditya.paragonmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CITRINE_ORE = new Item(new FabricItemSettings());
+
+    public static void registerItems() {
+        Registry.register(Registries.ITEM, new Identifier("paragonmod", "citrine_ore"), CITRINE_ORE);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(CITRINE_ORE);
+        });
+    }
+}
